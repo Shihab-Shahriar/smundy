93ae689de3a66fbf1ad60c63dd25c2821b73de70
shihab-shahriar <bsse0703@iit.du.ac.bd>
2023-04-24 15:48:51 -0400

    removed unnecessary files

--- vars.sh
+++
@@ -1,8 +0,0 @@
-export CUDA_HOME=/usr/local/cuda-12.1
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-12.1/lib64:/usr/local/cuda-12.1/extras/CUPTI/lib64
-export PATH=$PATH:$CUDA_HOME/bin
-
-export OMP_PROC_BIND=spread
-export OMP_PLACES=threads
-
-export KOKKOS_INSTALL_DIR=/home/shihab/src/smundy/build/dep/Kokkos
